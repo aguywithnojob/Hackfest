fbe779594a04a8c238cb6e20fa62a668dfdb2f39
ctadel <nep.prajwal@gmail.com>
2022-10-14 09:08:51 +0545

    Create create_a_file.py

---
+++ create_a_file.py
@@ -0,0 +1,49 @@
+def retrieve_cmk(desc):
+    """Retrieve an existing KMS CMK based on its description
+
+    :param desc: Description of CMK specified when the CMK was created
+    :return Tuple(KeyId, KeyArn) where:
+        KeyId: CMK ID
+        KeyArn: Amazon Resource Name of CMK
+    :return Tuple(None, None) if a CMK with the specified description was
+    not found
+    """
+
+    # Retrieve a list of existing CMKs
+    # If more than 100 keys exist, retrieve and process them in batches
+    kms_client = boto3.client('kms')
+    try:
+        response = kms_client.list_keys()
+    except ClientError as e:
+        logging.error(e)
+        return None, None
+
+    done = False
+    while not done:
+        for cmk in response['Keys']:
+            # Get info about the key, including its description
+            try:
+                key_info = kms_client.describe_key(KeyId=cmk['KeyArn'])
+            except ClientError as e:
+                logging.error(e)
+                return None, None
+
+            # Is this the key we're looking for?
+            if key_info['KeyMetadata']['Description'] == desc:
+                return cmk['KeyId'], cmk['KeyArn']
+
+        # Are there more keys to retrieve?
+        if not response['Truncated']:
+            # No, the CMK was not found
+            logging.debug('A CMK with the specified description was not found')
+            done = True
+        else:
+            # Yes, retrieve another batch
+            try:
+                response = kms_client.list_keys(Marker=response['NextMarker'])
+            except ClientError as e:
+                logging.error(e)
+                return None, None
+
+    # All existing CMKs were checked and the desired key was not found
+    return None, None
